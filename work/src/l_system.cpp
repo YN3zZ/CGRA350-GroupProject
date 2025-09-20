5d9d7d9eabc2ac8b1470b84e8e6dee77742b5a63
Ylli Neziri <neziryll@students.zhaw.ch>
2025-09-20 17:49:56 +1200

    Implement basic turtle graphics interpretation for L-System

--- work/src/l_system.cpp
+++ work/src/l_system.cpp
@@ -32,7 +32,73 @@ string LSystem::generateString() {
 
 gl_mesh LSystem::generateTreeMesh(const string& lSystemString) {
     mesh_builder mb;
-    // We'll implement this in the next step
-    cout << "Generated L-System string: " << lSystemString.substr(0, 50) << "..." << endl;
+    
+    // Stack for push/pop operations
+    stack<TurtleState> stateStack;
+    
+    // Initial turtle state
+    TurtleState turtle;
+    turtle.position = vec3(0, 0, 0);
+    turtle.direction = vec3(0, 1, 0); // Growing upward
+    turtle.rotation = mat4(1.0f);
+    
+    unsigned int vertexIndex = 0;
+    
+    for (char c : lSystemString) {
+        switch (c) {
+            case 'F': {
+                // Draw a branch segment
+                vec3 startPos = turtle.position;
+                vec3 endPos = turtle.position + turtle.direction * stepLength;
+                
+                // Create a simple cylinder for the branch
+                mesh_vertex v1, v2;
+                v1.pos = startPos;
+                v1.norm = normalize(turtle.direction);
+                v1.uv = vec2(0, 0);
+                
+                v2.pos = endPos;
+                v2.norm = normalize(turtle.direction);
+                v2.uv = vec2(1, 0);
+                
+                mb.push_vertex(v1);
+                mb.push_vertex(v2);
+                mb.push_indices({vertexIndex, vertexIndex + 1});
+                vertexIndex += 2;
+                
+                // Move turtle to end position
+                turtle.position = endPos;
+                break;
+            }
+            case '+': {
+                // Rotate around Z axis (positive)
+                mat4 rotation = rotate(mat4(1.0f), radians(angle), vec3(0, 0, 1));
+                turtle.rotation = rotation * turtle.rotation;
+                turtle.direction = vec3(rotation * vec4(turtle.direction, 0));
+                break;
+            }
+            case '-': {
+                // Rotate around Z axis (negative)
+                mat4 rotation = rotate(mat4(1.0f), radians(-angle), vec3(0, 0, 1));
+                turtle.rotation = rotation * turtle.rotation;
+                turtle.direction = vec3(rotation * vec4(turtle.direction, 0));
+                break;
+            }
+            case '[': {
+                // Push current state
+                stateStack.push(turtle);
+                break;
+            }
+            case ']': {
+                // Pop state
+                if (!stateStack.empty()) {
+                    turtle = stateStack.top();
+                    stateStack.pop();
+                }
+                break;
+            }
+        }
+    }
+    
     return mb.build();
 }
