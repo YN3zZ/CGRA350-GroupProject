69d1649470ffe8f6ac66ea5a9caa8b6fe7ecdd71
Ylli Neziri <neziryll@students.zhaw.ch>
2025-10-07 17:50:39 +1300

    Added the sun as light point

--- work/res/shaders/skybox_frag.glsl
+++ work/res/shaders/skybox_frag.glsl
@@ -4,7 +4,14 @@ out vec4 FragColor;
 in vec3 TexCoords;
 
 uniform samplerCube skybox;
+uniform vec3 atmosphereColor;  // Sky tint color based on sun position
+uniform float atmosphereBlend; // How much to blend
 
 void main() {
-    FragColor = texture(skybox, TexCoords);
+    vec4 skyboxColor = texture(skybox, TexCoords);
+
+    // Blend skybox texture
+    vec3 finalColor = mix(skyboxColor.rgb, atmosphereColor, atmosphereBlend);
+
+    FragColor = vec4(finalColor, 1.0);
 }
