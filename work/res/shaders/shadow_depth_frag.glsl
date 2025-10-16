f84a57ba2f2b6bb167ae34ac9ed30936a5927aeb
Ylli Neziri <neziryll@students.zhaw.ch>
2025-10-16 22:31:58 +1300

    Improve leaf performance and remove unused assets

--- work/res/shaders/shadow_depth_frag.glsl
+++ work/res/shaders/shadow_depth_frag.glsl
@@ -1,5 +1,17 @@
 #version 330 core
 
+uniform sampler2D uLeafTexture;
+uniform bool uRenderingLeaves;
+
+in vec2 vTexCoord;
+
 void main() {
+    if (uRenderingLeaves) {
+        vec4 texColor = texture(uLeafTexture, vTexCoord);
+        if (texColor.a < 0.1) {
+            discard;
+        }
+    }
+
     // Depth is automatically written to GL_DEPTH_ATTACHMENT
 }
